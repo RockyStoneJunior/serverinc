e403d043675275696559413c57ada3d2838efe00
Rocky Stone <stone@511697856@qq.com>
2018-07-12 15:01:34 +0800

    add pthread_create to utilize multithread, when client closes the connection, infinit loop print message

--- server.c
+++ server.c
@@ -8,6 +8,17 @@
 #include <netdb.h>
 #include <sys/socket.h>
 
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+
+struct arg{
+	int serverfd;
+	int clientfd;
+};
+
+void *connetion_process(void *);
+
 int main(int argc, char **argv)
 {
 	char recv_buff[1024];
@@ -15,6 +26,10 @@ int main(int argc, char **argv)
 	int response_len = strlen(response);
 
 	struct sockaddr_in servaddr;
+
+	struct arg *connection_thread_arg;
+	
+	pthread_t connection_process_thread;
 
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -25,11 +40,39 @@ int main(int argc, char **argv)
 	listen(servfd, 5);
 
 	int servaddr_len = sizeof(servaddr);
-	int clientfd = accept(servfd, (struct sockaddr *)&servaddr, &servaddr_len);
 
-	int recv_len = read(clientfd, recv_buff, 1024);
-	write(clientfd, response, response_len);
-	write(STDOUT_FILENO, recv_buff, recv_len);
+	while(1)
+	{
+		int clientfd = accept(servfd, (struct sockaddr *)&servaddr, &servaddr_len);
+
+		connection_thread_arg = malloc(sizeof(struct arg));
+		connection_thread_arg->serverfd = servfd;
+		connection_thread_arg->clientfd = clientfd;
+		pthread_create(&connection_process_thread, NULL, connetion_process, connection_thread_arg);
+	}
 	
 	return 0;
+}
+
+void *connetion_process(void *arg)
+{	
+	char *recv_buff[1024];
+	int recv_len;
+	
+	pid_t pid = syscall(SYS_gettid);
+	
+	int serverfd = ((struct arg*)arg)->serverfd;
+	int clientfd = ((struct arg*)arg)->clientfd;
+
+	while(1)
+	{
+		while((recv_len = read(clientfd, recv_buff, 1024)) > 0)
+		{
+			write(STDOUT_FILENO, recv_buff, recv_len);
+		}
+		
+		printf("%d: No message now!\r\n",pid);
+	}
+
+	return NULL;
 }
