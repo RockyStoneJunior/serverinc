54eb264a9db920b3eab92e75fbb01c808efa65b4
Rocky Stone <stone@511697856@qq.com>
2018-07-12 15:37:20 +0800

    if quit command get, then quit the infinite loop and close the socket

--- server.c
+++ server.c
@@ -28,13 +28,13 @@ int main(int argc, char **argv)
 	struct sockaddr_in servaddr;
 
 	struct arg *connection_thread_arg;
-	
+
 	pthread_t connection_process_thread;
 
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(8888);
-	
+
 	int servfd = socket(AF_INET, SOCK_STREAM, 0);
 	bind(servfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 	listen(servfd, 5);
@@ -50,29 +50,33 @@ int main(int argc, char **argv)
 		connection_thread_arg->clientfd = clientfd;
 		pthread_create(&connection_process_thread, NULL, connetion_process, connection_thread_arg);
 	}
-	
+
 	return 0;
 }
 
 void *connetion_process(void *arg)
 {	
-	char *recv_buff[1024];
+	char recv_buff[1024];
 	int recv_len;
-	
+
 	pid_t pid = syscall(SYS_gettid);
-	
+
 	int serverfd = ((struct arg*)arg)->serverfd;
 	int clientfd = ((struct arg*)arg)->clientfd;
 
-	while(1)
+	while((recv_len = read(clientfd, recv_buff, 1024)) > 0)
 	{
-		while((recv_len = read(clientfd, recv_buff, 1024)) > 0)
-		{
-			write(STDOUT_FILENO, recv_buff, recv_len);
-		}
+		write(STDOUT_FILENO, recv_buff, recv_len);
 		
-		printf("%d: No message now!\r\n",pid);
+		if(!strncmp(recv_buff, "!quit ",6))
+		{
+			break;
+		}
 	}
+
+	printf("%d: thread exit now!\r\n",pid);
+	write(clientfd, "Bye!\r\n", 6);
+	close(clientfd);
 
 	return NULL;
 }
